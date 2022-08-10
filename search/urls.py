1a82e14281b1113f625d858032592aa901f5bd5a
Junsu Min <sunsuking@gmail.com>
2022-08-10 15:30:37 +0900

    Search 관련 수정 url 추가

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from search.views import SearchListAPIView, SearchWordListAPIView, SearchWordRankListAPIView
+from search.views import SearchListAPIView, SearchWordListAPIView, SearchWordRankListAPIView, SearchWordDeleteAPIView
 
 urlpatterns = [
     path('', SearchListAPIView.as_view()),
     path('history/', SearchWordListAPIView.as_view()),
+    path('history/<int:pk>/', SearchWordDeleteAPIView.as_view()),
     path('rank/', SearchWordRankListAPIView.as_view()),
 ]
