e38f230d8b571ea9f330ecded6db3c9cdc69d577
junsu <sunsuking@gmail.com>
2022-08-29 14:06:24 +0900

    - /products/<int:pk>/reviews/ : 특정 상품에 리뷰 관련 API 수정 - 프로필 정보 추가
    ProductSimpleSerializer -> is_bucket 추가하여 로그인된 사용자가 장바구니에 존재하는지 확인

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,45 @@
 from rest_framework import serializers
 
-from .models import Profile, Platform, Bucket, Address, Point, Notify
+from accounts.models import Address, Profile, Point, Notify, Platform, Bucket
+from products.models import Review
 from products.serializers import ProductSimpleSerializer
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    is_main = serializers.BooleanField(read_only=True)
+
+    class Meta:
+        model = Address
+        fields = ['id', 'is_main', 'name', 'recipient', 'address', 'road_name_address',
+                  'detail_address', 'zip_code', 'phone_number', 'memo']
+
+    def create(self, validated_data):
+        user = self.context.get("request").user
+        profile = Profile.get_profile_or_exception(user.profile.id)
+        if not Address.objects.filter(profile=profile, is_main=True).exists():
+            return Address.objects.create(profile=profile, **validated_data, is_main=True)
+        return Address.objects.create(profile=profile, **validated_data)
+
+
+class PointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Point
+        fields = ['id', 'memo', 'point', 'is_use', 'created']
+
+
+class ProfilePointSerializer(serializers.ModelSerializer):
+    point_history = PointSerializer(source='points', many=True)
+
+    class Meta:
+        model = Profile
+        fields = ['point', 'point_history']
+
+
+class NotifySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notify
+        fields = ['is_store', 'is_community', 'is_event']
 
 
 class PlatformSerializer(serializers.ModelSerializer):
@@ -44,38 +82,9 @@ class ProfileSummarySerializer(serializers.ModelSerializer):
         fields = ['favorites', 'buckets', 'point']
 
 
-class AddressSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    is_main = serializers.BooleanField(read_only=True)
+class ReviewListSerializer(serializers.ModelSerializer):
+    profile = ProfileSimpleSerializer()
 
     class Meta:
-        model = Address
-        fields = ['id', 'is_main', 'name', 'recipient', 'address', 'road_name_address',
-                  'detail_address', 'zip_code', 'phone_number', 'memo']
-
-    def create(self, validated_data):
-        user = self.context.get("request").user
-        profile = Profile.get_profile_or_exception(user.profile.id)
-        if not Address.objects.filter(profile=profile, is_main=True).exists():
-            return Address.objects.create(profile=profile, **validated_data, is_main=True)
-        return Address.objects.create(profile=profile, **validated_data)
-
-
-class PointSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Point
-        fields = ['id', 'memo', 'point', 'is_use', 'created']
-
-
-class ProfilePointSerializer(serializers.ModelSerializer):
-    point_history = PointSerializer(source='points', many=True)
-
-    class Meta:
-        model = Profile
-        fields = ['point', 'point_history']
-
-
-class NotifySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notify
-        fields = ['is_store', 'is_community', 'is_event']
+        model = Review
+        fields = ['id', 'profile', 'created', 'star', 'description']
