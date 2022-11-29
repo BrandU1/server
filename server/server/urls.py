5ae27e04ecf411b739a9e854942ada20ea633a17
junsu <sunsuking@gmail.com>
2022-11-29 09:01:53 +0900

    swagger 관련 설정 변경

--- server/server/urls.py
+++ server/server/urls.py
@@ -44,7 +44,7 @@ if settings.DEBUG:
     urlpatterns += [path('admin/', admin.site.urls)]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [
-        path('docs<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+        path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     ]
