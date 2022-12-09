104101786f9d3356f84ba4b7b94325a44f49c8ec
junsu <sunsuking@gmail.com>
2022-12-10 00:21:31 +0900

    스테이징 서버 관련 설정

--- server/server/settings/__init__.py
+++ server/server/settings/__init__.py
@@ -5,5 +5,8 @@ SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
 if not SETTINGS_MODULE or SETTINGS_MODULE == 'server.settings':
     from .local import *
 
+elif SETTINGS_MODULE == 'server.settings.staging':
+    from .staging import *
+
 elif SETTINGS_MODULE == 'server.settings.production':
     from .production import *
