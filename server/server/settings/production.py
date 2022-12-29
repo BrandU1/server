ffb36cd3e03d76478d8a8a0a70e4589277c48e2e
sunsuking <sunsuking@gmail.com>
2022-12-29 23:22:17 +0900

    redis 관련 설정 변경

--- server/server/settings/production.py
+++ server/server/settings/production.py
@@ -100,7 +100,7 @@ CORS_ORIGIN_WHITELIST = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
+        'LOCATION': 'redis://redis:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
